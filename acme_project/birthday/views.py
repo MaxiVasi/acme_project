f453cb3f78cbaf84dc5368200c5462d2396954c9
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-09-04 13:21:58 +0300

    Complete lesson 3 thema 1-4

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,14 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
 # Импортируем модель дней рождения.
 from .models import Birthday
+from django.core.paginator import Paginator
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
+
+
+# функция дублирует функцию birthday. будет обьеденена с ней.
+# def edit_birthday(request, pk):
+    # Находим запрошенный объект для редактирования по первичному ключу
+    # или возвращаем 404 ошибку, если такого объекта нет.
+#    instance = get_object_or_404(Birthday, pk=pk)
+    # Связываем форму с найденным объектом: передаём его в аргумент instance.
+#    form = BirthdayForm(request.POST or None, instance=instance)
+    # Всё остальное без изменений.
+#    context = {'form': form}
+    # Сохраняем данные, полученные из формы, и отправляем ответ:
+#    if form.is_valid():
+#        form.save()
+#        birthday_countdown = calculate_birthday_countdown(
+#            form.cleaned_data['birthday']
+#        )
+#        context.update({'birthday_countdown': birthday_countdown})
+#    return render(request, 'birthday/birthday.html', context)
+
+
+# Добавим опциональный параметр pk.
+def birthday(request, pk=None):
+    # Если в запросе указан pk (если получен запрос на редактирование объекта):
+    if pk is not None:
+        # Получаем объект модели или выбрасываем 404 ошибку.
+        instance = get_object_or_404(Birthday, pk=pk)
+    # Если в запросе не указан pk
+    # (если получен запрос к странице создания записи):
+    else:
+        # Связывать форму с объектом не нужно, установим значение None.
+        instance = None
+    # Передаём в форму либо данные из запроса, либо None.
+    # В случае редактирования прикрепляем объект модели.
+    form = BirthdayForm(
+        request.POST or None,
+        # Файлы, переданные в запросе, указываются отдельно.
+        files=request.FILES or None,
+        instance=instance
+    )
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
@@ -26,8 +80,18 @@ def birthday(request):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
+    # Получаем все объекты модели Birthday из БД сортированные по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 5)
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
     # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context) 
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context)
