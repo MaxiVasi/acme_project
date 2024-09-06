41f8123ae000ba96ab391da2691a535195ff35a8
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-09-06 13:14:03 +0300

    Before Sprint 10

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     # path('', views.birthday, name='create'),
     # Использование CBV маршрут связывают с CBV: имя_класса.as_view()
     path('', views.BirthdayCreateView.as_view(), name='create'),
-    # Новый маршрут.
     # path('list/', views.birthday_list, name='list'),
     # Использование CBV маршрут связывают с CBV: имя_класса.as_view()
     path('list/', views.BirthdayListView.as_view(), name='list'),
+    path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
     # path('<int:pk>/edit/', views.birthday, name='edit'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     # path('<int:pk>/delete/', views.delete_birthday, name='delete'),
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -6,45 +6,52 @@ from .utils import calculate_birthday_countdown
 # Импортируем модель дней рождения.
 from .models import Birthday
 from django.core.paginator import Paginator
-
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+    )
 from django.urls import reverse_lazy
 
 
 # Создаём миксин.
-class BirthdayMixin:
+# class BirthdayMixin:
+#    model = Birthday
+#    success_url = reverse_lazy('birthday:list')
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
 
-# Создаём миксин.
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-
-
-# Наследуем класс от встроенного ListView - Это CBV заменяет def birthday_list:
-class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
+class BirthdayDetailView(DetailView):
     model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
-    ordering = 'id'
-    # ...и даже настройки пагинации:
-    paginate_by = 5
+    # template_name_suffix = '_detail'
 
-
-# Добавляем миксин первым по списку родительских классов.
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-# Добавляем миксин первым по списку родительских классов.
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
 
 
 # Создан класс MIXIN.
