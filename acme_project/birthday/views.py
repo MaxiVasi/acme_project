f27b332a01b0ae3f75f2d4e6fc88391d465a613e
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-09-12 09:46:50 +0300

    final

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -28,8 +28,14 @@ class OnlyAuthorMixin(UserPassesTestMixin):
 
 class BirthdayListView(ListView):
     model = Birthday
+    # По умолчанию этот класс
+    # выполняет запрос queryset = Birthday.objects.all(),
+    # но мы его переопределим:
+    queryset = Birthday.objects.prefetch_related(
+        'tags'
+    ).select_related('author')
     ordering = 'id'
-    paginate_by = 10
+    paginate_by = 5
 
 
 # Наследуем BirthdayCreateView от CreateView и от миксина LoginRequiredMixin:
